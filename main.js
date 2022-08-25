9642198587a7f02a576fcbc2fed8bc8318c7487a
NihadPanahov <nihadpanah03@gmail.com>
2022-08-25 17:38:10 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+// function difference(n)
+//  {
+//     if (n <= 13)
+//         return 13 - n;
+//     else
+//         return (n - 13) * 2;
+//  }
+// console.log(difference(15))
+// console.log(difference(30))
+
+
+
+
+// function plus(n,m) {
+//     if (n!==m)
+//         return n+m;
+//     else
+//         return (n+m)*3;
+// }
+// console.log(plus(5,10))
+// console.log(plus(5,5))
+
+
+
+
+// function difference(x) {
+//     if (x <= 19)
+//         return 19 - x;
+//     else 
+//         return (x - 19) * 3;
+// }
+// console.log(difference(9))
+// console.log(difference(20))
+
+
+
+
+// function fifty(x,y) {
+//     return ((x == 50 || y == 50) || (x + y ==50));
+// }
+// console.log(fifty(50,9));
+// console.log(fifty(20,50));
+// console.log(fifty(20,40));
+// console.log(fifty(10,40));
+
+
+
+
+// function sual20(x , y) {
+//     return ((x > 0 && y < 0) || (x < 0 && y > 0))
+// }
+// console.log(sual20(10 , 20))
+// console.log(sual20 (-5 , 15))
